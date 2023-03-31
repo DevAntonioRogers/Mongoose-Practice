bf1b4452f2742e246a6116433e0e07da739940de
DevAntonioRogers <ar919061493@gmail.com>
2023-03-31 18:57:07 -0400

    Configured Options

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -12,18 +13,32 @@ connect.then(() => {
 
   console.log('Connected correctly to server');
 
-  const newCampsite = new Campsite({
+  Campsite.create({
     name: 'React Lake Campground',
     description: 'test'
-  });
-
-  newCampsite.save()
+  })
     .then(campsite => {
       console.log(campsite);
-      return Campsite.find();
+
+      return Campsite.findByIdAndUpdate(campsite._id, {
+        $set: { description: 'Updated Test Document' }
+      }, {
+        new: true
+      });
     })
-    .then(campsites => {
-      console.log(campsites);
+    .then(campsite => {
+      console.log(campsite);
+
+      campsite.comments.push({
+        rating: 5,
+        text: 'What a magnificent view!',
+        author: 'Tinus Lorvaldes'
+      });
+
+      return campsite.save();
+    })
+    .then(campsite => {
+      console.log(campsite);
       return Campsite.deleteMany();
     })
     .then(() => {
--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
 const campsiteSchema = new Schema({
   name: {
     type: String,
@@ -10,10 +29,12 @@ const campsiteSchema = new Schema({
   description: {
     type: String,
     required: true
-  }
+  },
+  comments: [commentSchema]
 }, {
   timestamps: true
 });
+
 
 const Campsite = mongoose.model('Campsite', campsiteSchema);
 
